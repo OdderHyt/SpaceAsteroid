63a752dbaec6fc8b2e5a46b79ae54e9b2a305c39
Marcus <mhytte18@student.aau.dk>
2021-07-02 00:54:17 +0200

    Update .vsconfig, Player.prefab, and 7 more files...

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float MovementSpeed = 5, TurningSpeed = 20;
+
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    public void Forward() {
+        rb.velocity += (Vector2)transform.up * Time.deltaTime * MovementSpeed;
+    }
+
+    public void Backward() {
+        rb.velocity -= (Vector2)transform.up * Time.deltaTime * MovementSpeed;
+    }
+
+    public void TurnLeft() {
+        transform.Rotate(0f,0f, Time.deltaTime * TurningSpeed);
+    }
+
+    public void TurnRight() {
+        transform.Rotate(0f,0f, -Time.deltaTime * TurningSpeed);
+
+    }
+}
